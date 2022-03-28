25e8cc4993d03d258c12f21afa0a6bb1bb0c6d04
hugosenari <hugosenari@gmail.com>
2022-03-27 21:50:06 -0300

    chore: re index golib

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-golib-master.ref   = "refs/heads/master";
   inputs.src-golib-master.owner = "stefantalpalaru";
   inputs.src-golib-master.repo  = "golib-nim";
-  inputs.src-golib-master.dir   = "";
   inputs.src-golib-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
